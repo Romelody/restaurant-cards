dc6d6482b405ee63e902af436e5650b1e3ab47c7
Melody <romelody.calde@gmail.com>
2020-05-09 22:00:18 -0300

    First Commit

---
+++ src/components/Restaurant.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="restaurant" >
+        <div class="restaurant" >
+            {{name}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Restaurant',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style>
+ #restaurant{
+    display: flex;
+    flex-direction: row;
+ }
+
+.restaurant{
+    width: 33%;
+    height: 300px;
+    background: powderblue;
+    border-radius: 30px;
+
+}
+
+</style>
---
+++ src/components/Restaurants.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="restaurants" class="container">
+    <h1>Restaurants</h1>
+    <Restaurant
+      v-for="restaurant in asian_restaurants"
+      :name=restaurant.name
+      :key="restaurant.id" />
+  </div>
+</template>
+
+<script>
+import Restaurant from './Restaurant.vue'
+export default {
+  components: {
+    Restaurant
+  },
+  data () {
+    return {
+      asian_restaurants: [
+        {
+          name: 'Nobiru',
+          rating: 4,
+          type: 'Asian Food',
+          id: '001'
+        },
+        {
+          name: 'Nippori',
+          rating: 5,
+          type: 'Asian Food',
+          id: '002'
+        }
+      ],
+      burger_restaurants: [
+        {
+          name: 'Burger King',
+          rating: 4,
+          type: 'Burger',
+          id: '004'
+        },
+        {
+          name: 'Doggs',
+          rating: 5,
+          type: 'Burger',
+          id: '003'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+  width: 90%;
+  height: 500px;
+  margin: 0 auto;
+  background: peachpuff;
+  border-radius: 30px;
+}
+
+h1{
+  padding: 30px;
+  text-transform: uppercase;
+}
+</style>
